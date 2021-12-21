874fc97081cf62d3830ab0023e0842153963adb8
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2021-12-21 16:43:02 +0100

    Added getter and setter
    Added getter and setter to the Formular Model.

--- Classes/Domain/Model/Formular.php
+++ Classes/Domain/Model/Formular.php
@@ -16,16 +16,76 @@ class Formular extends AbstractEntity
 
     protected $password = '';
 
-    protected $birthdate = '';
+    protected $birthdate;
 
     public function __construct(string $firstname = '', string $lastname = '', string $email = '', 
-                                string $profilepicture = '', string $password = '', date $birthdate = '' ): void
+                                string $profilepicture = '', string $password = '', date $birthdate)
     {
         $this->setFirstname($firstname);
         $this->setLastname($lastname);
         $this->setEmail($email);
         $this->setProfilepicture($profilepicture);
-        $this->setpassword($password);
+        $this->setPassword($password);
         $this->setBirthdate($birthdate);
+    }
+
+    public function setFirstname(string $firstname): void
+    {
+        $this->firstname = $firstname;
+    }
+
+    public function getFirstname():string
+    {
+        return $this->firstname;
+    }
+
+    public function setLastname(string $lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+    public function getLastname():string
+    {
+        return $this->lastname;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail():string
+    {
+        return $this->email;
+    }
+
+    public function setProfilepicture(string $profilepicture): void
+    {
+        $this->profilePicture = $profilepicture;
+    }
+
+    public function getProfilepicture():string
+    {
+        return $this->profilepicture;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getPassword():string
+    {
+        return $this->password;
+    }
+
+    public function setBirthdate(date $birthdate): void
+    {
+        $this->birthdate = $birthdate;
+    }
+
+    public function getPassword():date
+    {
+        return $this->birthdate;
     }
 }
