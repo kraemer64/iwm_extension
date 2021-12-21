a32850d6cba90357767adf5ad31ccbcef0a1f7ed
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2021-12-21 19:01:26 +0100

    Update ext_tables.sql
    Changed data types of the birthdate column.

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE tx_iwmextension_domain_model_formular (
    email varchar(40) DEFAULT '' NOT NULL,
    profilepicture varchar(255) DEFAULT '',
    password varchar(20) DEFAULT '' NOT NULL,
-   birthdate date DEFAULT '' NOT NULL,
+   birthdate varchar(30) DEFAULT '' NOT NULL,
 );
 
 CREATE TABLE tx_iwmextension_user (
@@ -20,3 +20,4 @@ CREATE TABLE tx_iwmextension_login (
    email varchar(40) DEFAULT '' NOT NULL,
    password varchar(20) DEFAULT '' NOT NULL,
 );
+
