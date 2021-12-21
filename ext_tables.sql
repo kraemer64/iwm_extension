ec93884c37c4f58f94ae122d45a5425ecd29de1f
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2021-12-21 18:29:09 +0100

    Update ext_tables.sql
    Added another table called tx_iwmextension_user to save the form inputs

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,3 +6,12 @@ CREATE TABLE tx_iwmextension_domain_model_formular (
    password varchar(20) DEFAULT '' NOT NULL,
    birthdate date DEFAULT '' NOT NULL,
 );
+
+CREATE TABLE tx_iwmextension_user (
+   firstname varchar(30) DEFAULT '',
+   lastname varchar(30) DEFAULT '',
+   email varchar(40) DEFAULT '' NOT NULL,
+   profilepicture varchar(255) DEFAULT '',
+   password varchar(20) DEFAULT '' NOT NULL,
+   birthdate date DEFAULT '' NOT NULL,
+);
