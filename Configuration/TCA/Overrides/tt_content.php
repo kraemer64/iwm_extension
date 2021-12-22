3dbf4e3749ec881d3c95bd9783ca8585692cce69
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2021-12-22 15:52:59 +0100

    Create tt_content.php
    Created tt_content.php to register the plugin to have it appear as a selectable element.

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,8 @@
+<?php
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'MyVendor.IwmExtension',
+    'IwmFormExtension',
+    'Iwm Formular Extension',
+    'EXT:iwm_extension/Resources/Public/Icons/Extension.svg'
+);
