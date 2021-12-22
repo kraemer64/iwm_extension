94fd2a084cecb3064fe84c240ccd5f0d0cbe72bc
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2021-12-22 01:16:15 +0100

    Revert "Update FormController.php"
    This reverts commit 090df5c034b46a7cfcdcfb14e1d3a8b5d8f0d56f.

--- Classes/Controller/FormController.php
+++ Classes/Controller/FormController.php
@@ -4,11 +4,6 @@ namespace MyVendor\IwmExtension\Controller;
 
 use MyVendor\IwmExtension\Domain\Repository\FormRepository;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
-<<<<<<< Updated upstream
-=======
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Database\ConnectionPool;
->>>>>>> Stashed changes
 
 class FormController extends ActionController
 {
@@ -24,35 +19,11 @@ class FormController extends ActionController
         $this->formRepository = $formRepository;
     }
 
-<<<<<<< Updated upstream
     public function listAction()
-=======
-    public function listAction():void
->>>>>>> Stashed changes
     {
         $formlabels = $this->formRepository->findAll();
         $this->view->assign('labels', $formlabels);
 
         return $this->view->render();
     }
-<<<<<<< Updated upstream
-=======
-
-    public function insertUser($values):void
-    {
-        GeneralUtility::makeInstance(ConnectionPool::class)
-            ->getConnectionForTable('tx_iwmextension_user')
-                ->insert(
-                    'tx_iwmextension_user',
-                    [
-                        'firstname' => $values[firstname],
-                        'lastname' => $values[lastnamename],
-                        'firstname' => $values[email],
-                        'lastname' => $values[profilepicture],
-                        'firstname' => $values[password],
-                        'lastname' => $values[birthdate],
-                    ]
-                )
-    }
->>>>>>> Stashed changes
 }
